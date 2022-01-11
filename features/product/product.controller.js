cabdbb29f2540660c7010c606a2d25e0cbd11842
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2022-01-12 00:40:35 +0800

    basic auth and check user password

--- features/product/product.controller.js
+++ features/product/product.controller.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const authService = require('../auth/auth.service');
 const productService = require('./product.service');
 
 const productController = express.Router();
@@ -8,9 +9,26 @@ productController.get('/', (req, res, next) => {
     return res.status(401).setHeader('WWW-Authenticate', 'Basic').end();
   }
 
-  productService
-    .getProducts()
-    .then((products) => res.json(products))
+  const [, credentialsRaw] = req.headers.authorization.split(' ');
+  const [username, password] = Buffer.from(credentialsRaw, 'base64')
+    .toString('ascii')
+    .split(':');
+
+  authService
+    .isValid({
+      username,
+      password,
+    })
+    .then((isValid) => {
+      if (!isValid) {
+        return res.status(401).setHeader('WWW-Authenticate', 'Basic').end();
+      }
+
+      productService
+        .getProducts()
+        .then((products) => res.json(products))
+        .catch((err) => next(err));
+    })
     .catch((err) => next(err));
 });
 
