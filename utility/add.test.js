e4ed20c5ec3a21c07aad2066ec9d18246168f056
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2022-01-14 01:18:05 +0800

    remove test and expect function and use jest

--- utility/add.test.js
+++ utility/add.test.js
@@ -1,29 +1,7 @@
 const add = require('./add');
 
-function test(name, testCode) {
-  try {
-    testCode();
-    console.log(`Passed: ${name}`);
-  } catch (err) {
-    console.error(`Failed: ${name}`);
-    console.error(err);
-  }
-}
-
-function expect(value) {
-  function toBe(expected) {
-    if (value !== expected) {
-      throw new Error(`${value} does not equal to ${expected}`);
-    }
-  }
-
-  return {
-    toBe,
-  };
-}
-
 test('add 1 and 4', () => {
-  expect(add(1, 4)).toBe(4);
+  expect(add(1, 4)).toBe(5);
 });
 
 test('add 1 and 6', () => {
