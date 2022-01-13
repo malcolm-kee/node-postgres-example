44cf5a7f1b0095240f11306c72ac5e0b6231c880
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2022-01-14 01:17:23 +0800

    recreate test and expect function

--- utility/add.test.js
+++ utility/add.test.js
@@ -1,5 +1,31 @@
 const add = require('./add');
 
-test('add', () => {
-  expect(add(1, 4)).toBe(5);
+function test(name, testCode) {
+  try {
+    testCode();
+    console.log(`Passed: ${name}`);
+  } catch (err) {
+    console.error(`Failed: ${name}`);
+    console.error(err);
+  }
+}
+
+function expect(value) {
+  function toBe(expected) {
+    if (value !== expected) {
+      throw new Error(`${value} does not equal to ${expected}`);
+    }
+  }
+
+  return {
+    toBe,
+  };
+}
+
+test('add 1 and 4', () => {
+  expect(add(1, 4)).toBe(4);
+});
+
+test('add 1 and 6', () => {
+  expect(add(1, 6)).toBe(7);
 });
