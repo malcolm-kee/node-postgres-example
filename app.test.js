f41eba94cf63f1ba9da5e060ba828029a900d9c8
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2022-01-14 11:59:44 +0800

    add test for not found route

--- app.test.js
+++ app.test.js
@@ -1,7 +1,27 @@
+const supertest = require('supertest');
 const createApp = require('./app');
 
 test('it is defined', () => {
   const app = createApp();
 
   expect(app).toBeDefined();
+});
+
+test('it returns 404 for unmatch path', async () => {
+  const app = createApp();
+
+  const agent = supertest(app);
+
+  const defaultResult = await agent
+    .get('/non-exi3t')
+    .then((result) => result.text);
+
+  expect(defaultResult.includes('Page Not Found')).toBe(true);
+
+  const jsonResult = await agent
+    .get('/non-exi3t')
+    .set('Accept', 'application/json')
+    .then((result) => result.body);
+
+  expect(jsonResult).toStrictEqual({ error: 'Not found' });
 });
